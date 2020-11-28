1c581b056558769869b3b2649b55ef709572c1b8
Emil Persson <emilper1999@gmail.com>
2020-11-28 18:10:30 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+        <div className="container">
+          <h2>MERN-Stack Todo App</h2>
+          <Route path="/" exact component={TodosList}/>
+          <Route path="/edit/:id" component={EditTodo}/>
+          <Route path="/create" component={CreateTodo}/>
+        </div>
+    </Router>
   );
 }
 
